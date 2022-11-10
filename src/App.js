640b145897cfdda2399749056cb89c141b261a03
Анастасия <MrsDante@MacBook-Pro-Anastasia.local>
2022-11-10 22:25:45 +0300

    .

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
 import './App.css';
 import Navbar from './components/layout/Navbar';
+import Index from './components/layout/Index';
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Routes, Route } from "react-router-dom";
+
+//const Index = React.lazy(() => import("./components/layout/Index"));
 
 function App() {
+  //<Route path='/' element={<Home/>} />
+
+  /*
+    
+      <React.Fragment>
+        <Navbar />
+        <Routes>
+        <Route exact path="/" element={<Index />} />
+        <Route exact path="/" element={<Index />} />
+        </Routes>
+      </React.Fragment>
+  */
+
+
   return (
-    <div className="App">
-      <Navbar />
-    </div>
+<React.Fragment>
+  <Navbar />
+
+    <Router>
+       <Routes>
+       <Route exact path="/" element={<Index />} />
+       </Routes>
+       </Router>
+  </React.Fragment>
   );
 }
 
